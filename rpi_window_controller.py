634bc781db6e807039aa0f9809dcdb2a0f3cdad7
Grusinator <grusinator@gmail.com>
2019-10-08 19:20:51 +0200

    fixing errors

--- rpi_window_controller.py
+++ rpi_window_controller.py
@@ -1,15 +1,18 @@
 
 from flask import Flask
+from WindowController import WindowController
 
 app = Flask(__name__)
 
-if __name__ == '__main__':
-	@app.route('/test/')
-	def running_test():
-		controller = WindowController()
-		controller.run_test()
-		return "Opening Window"
+@app.route('/test/')
+def running_test():
+	controller = WindowController()
+	controller.run_test()
+	text = "running test"
+	print(text)
+	return text
 
+if __name__ == '__main__':
     app.run()
 
 	
