4677237263f2b623fa28e6ac89345ad782f336d3
Grusinator <grusinator@gmail.com>
2019-10-08 18:25:58 +0200

    set to allways high

--- window_controller.py
+++ window_controller.py
@@ -4,14 +4,5 @@ import time
 gpio.setmode(gpio.BCM)
 gpio.setup(18, gpio.OUT)
 
-while True:
-    gpio.output(18, gpio.HIGH)
-
-	
-	if passcode == "Awesome":
-	    gpio.output(18, gpio.LOW)
-		time.sleep(4)
-	
-	else:
-	    gpio.output(18, gpio.HIGH)
-		print("Wrong Password!")
+#while True:
+gpio.output(18, gpio.HIGH)
