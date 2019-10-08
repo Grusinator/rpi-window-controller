19ade128a3d141c33477fc9dd5afcc510cb4490d
Grusinator <grusinator@gmail.com>
2019-10-08 20:43:06 +0200

    Async methods

--- WindowController.py
+++ WindowController.py
@@ -1,37 +1,44 @@
 import RPi.GPIO as gpio
 import time
 
+import asyncio
+
+async def count():
+    print("One")
+    await asyncio.sleep(1)
+    print("Two")
 
 class WindowController():
     def __init__(self):
         self.channel1 = 40
         self.channel2 = 38
-        self.opening_time = 50
+        self.opening_time = 15
+        self.test_time = 5
 
         gpio.setmode(gpio.BOARD)
         gpio.setwarnings(False)
         gpio.setup(self.channel1, gpio.OUT, initial=gpio.LOW)
         gpio.setup(self.channel2, gpio.OUT, initial=gpio.LOW)
 
-    def run_test(self):
+    async def run_test(self):
         while True:
             self.set_forward()
-            time.sleep(10)
+            await self.wait(self.test_time)
             self.stop()
-            time.sleep(10)
+            await self.wait(self.test_time)
             self.set_backwards()
-            time.sleep(10)
+            await self.wait(self.test_time)
             self.stop()
-            time.sleep(10)
+            await self.wait(self.test_time)
 
-    def open_window(self):
+    async def open_window(self):
         self.set_forward()
-        time.sleep(self.opening_time)
+        await self.wait(self.opening_time)
         self.stop()
 
-    def close_window(self):
+    async def close_window(self):
         self.set_forward()
-        time.sleep(self.opening_time)
+        await self.wait(self.opening_time)
         self.stop()
 
     def set_forward(self):
@@ -45,3 +52,6 @@ class WindowController():
     def stop(self):
         gpio.output(self.channel1, gpio.LOW)
         gpio.output(self.channel2, gpio.LOW)
+
+    async def wait(self, wait_time):
+        await asyncio.sleep(wait_time)
--- rpi_window_controller.py
+++ rpi_window_controller.py
@@ -1,30 +1,35 @@
+import asyncio
 
 from flask import Flask
+
 from WindowController import WindowController
 
 app = Flask(__name__)
 controller = WindowController()
 
+
 @app.route('/test/')
 def running_test():
-	controller.run_test()
-	return "running test"
+    asyncio.run(controller.run_test())
+    return "running test"
+
 
 @app.route('/open/')
 def open_window():
-	controller.open_window()
-	return "opening"
+    asyncio.run(controller.open_window())
+    return "opening"
+
 
 @app.route('/close/')
 def close_window():
-	controller.close_window()
-	return "closing"
+    asyncio.run(controller.close_window())
+    return "closing"
+
 
 @app.route('/')
 def default():
-	return "default landing page"
+    return "default landing page"
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=80, host='0.0.0.0')
-
-	
