2263e4e4ac84074f3a459f52c90abcfe5b541831
Grusinator <grusinator@gmail.com>
2019-10-08 19:30:11 +0200

    debug mode

--- rpi_window_controller.py
+++ rpi_window_controller.py
@@ -1,18 +1,24 @@
 
 from flask import Flask
-from WindowController import WindowController
+# from WindowController import WindowController
 
 app = Flask(__name__)
 
 @app.route('/test/')
 def running_test():
-	controller = WindowController()
-	controller.run_test()
+	# controller = WindowController()
+	# controller.run_test()
 	text = "running test"
 	print(text)
 	return text
 
+@app.route('/')
+def default():
+	text = "default landing page"
+	print(text)
+	return text
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, port=80, host='0.0.0.0')
 
 	
